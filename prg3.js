55343e44f35a8b883795a92f72ea1c10977bced1
anudharshini-bt <anudharshini1104@gmail.com>
2024-07-07 16:09:58 +0530

    first commit

---
+++ prg3.js
@@ -0,0 +1,7 @@
+var score;
+score=150;
+console.log(score);
+
+var score2;
+score=250;
+console.log(score);
---
+++ prg4.js
@@ -0,0 +1,13 @@
+var score;
+score = 350;
+console.log(score);
+
+score=220;
+console.log("score");
+
+var score2;
+score2=340;
+console.log("score 2",score2);
+
+score = 200;
+console.log("value",score);
